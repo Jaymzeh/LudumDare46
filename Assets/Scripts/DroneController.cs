d78f3fcd3244742ec604d6d4476893d11578f388
James Smith <james_smith.games@live.co.uk>
2020-04-18 15:53:52 +0100

    got basic drone movement working. fixed gitignore

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+    public float forwardSpeed = 10f;
+
+    public float forwardTilt = 0f;
+    public float sideTilt = 0f;
+
+
+    void GetInput() {
+
+        if (Input.GetAxis("Vertical") == 0) {
+            forwardTilt = Mathf.Lerp(forwardTilt, 0, Time.deltaTime * 0.5f);
+        }
+        else
+            forwardTilt = Input.GetAxis("Vertical")*-40;
+        //    if (Input.GetAxis("Vertical") > 0) {
+        //    forwardTilt = Mathf.Lerp(forwardTilt, -45, Time.deltaTime * 1);
+        //}
+        //else
+        //    if (Input.GetAxis("Vertical") < 0) {
+        //    forwardTilt = Mathf.Lerp(forwardTilt, 45, Time.deltaTime * 1);
+        //}
+
+        if (Input.GetAxis("Horizontal") == 0) {
+            sideTilt = Mathf.Lerp(0, sideTilt, Time.deltaTime * 0.5f);
+        }
+        else
+            sideTilt = Input.GetAxis("Horizontal")*40;
+    }
+
+    public void Update() {
+
+        GetInput();
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,
+            Quaternion.Euler(forwardTilt, 0, sideTilt), 1);
+
+    }
+}
