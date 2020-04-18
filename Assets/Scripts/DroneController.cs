814be4036b5d936192240fc43642bb30ac01c7be
James Smith <james_smith.games@live.co.uk>
2020-04-18 19:25:57 +0100

    Added anim controller

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -8,48 +8,63 @@ public class DroneController : MonoBehaviour
 
     public float forwardTilt = 0f;
     public float sideTilt = 0f;
+    public float turning = 0;
 
-    float remainingPower = 100;
 
     Transform model;
+    Animator anim;
+
+    public float rotorBaseSpeed = 10;
+
 
     void Awake() {
         model = transform.GetChild(0);
+        anim = GetComponent<Animator>();
     }
 
     void GetInput() {
 
-        if (Input.GetAxis("Vertical") == 0) {
-            forwardTilt = Mathf.Lerp(forwardTilt, 0, Time.deltaTime * 1f);
-        }
-        else {
-            forwardTilt = Input.GetAxis("Vertical") * -40;
+        //if (Input.GetAxis("Vertical") == 0) {
+        //    forwardTilt = Mathf.Lerp(forwardTilt, 0, Time.deltaTime * 1f);
+        //}
+        //else {
+        //    forwardTilt = Input.GetAxis("Vertical") * -40;
 
-            if (forwardTilt > 0)
-                transform.Translate(Vector3.forward * forwardSpeed, Space.Self);
-            else
-                transform.Translate(Vector3.forward * -forwardSpeed, Space.Self);
-        }
+        //    if (forwardTilt > 0) {
+        //        transform.Translate(Vector3.forward * forwardSpeed, Space.Self);
+        //    }
+        //    else {
+        //        transform.Translate(Vector3.forward * -forwardSpeed, Space.Self);
+        //    }
+        //}
 
-        if (Input.GetAxis("Horizontal") == 0) {
-            sideTilt = Mathf.Lerp(0, sideTilt, Time.deltaTime * 1f);
-        }
-        else {
-            sideTilt = Input.GetAxis("Horizontal") * 40;
+        //if (Input.GetAxis("Horizontal") == 0) {
+        //    sideTilt = Mathf.Lerp(0, sideTilt, Time.deltaTime * 1f);
+        //}
+        //else {
+        //    sideTilt = Input.GetAxis("Horizontal") * 40;
 
-            if (sideTilt > 0)
-                transform.Translate(Vector3.right * -forwardSpeed/2, Space.Self);
-            else
-                transform.Translate(Vector3.right * forwardSpeed/2, Space.Self);
-        }
+        //    if (sideTilt > 0) {
+        //        transform.Translate(Vector3.right * -forwardSpeed / 2, Space.Self);
+        //    }
+        //    else {
+        //        transform.Translate(Vector3.right * forwardSpeed / 2, Space.Self);
+        //    }
+        //}
+
+        //model.rotation = Quaternion.RotateTowards(model.rotation,
+        //    Quaternion.Euler(forwardTilt, 0, sideTilt), 5);
+
+        
+
     }
 
     public void Update() {
 
         GetInput();
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
 
-        model.rotation = Quaternion.RotateTowards(model.rotation,
-            Quaternion.Euler(forwardTilt, 0, sideTilt), 5);
 
     }
 }
