e4bee2a39374d5df4778ce9333f44d3214eeebbc
James Smith <james_smith.games@live.co.uk>
2020-04-18 19:41:00 +0100

    Added animations to blades. Fixed turning.

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -8,7 +8,7 @@ public class DroneController : MonoBehaviour
 
     public float forwardTilt = 0f;
     public float sideTilt = 0f;
-    public float turning = 0;
+    public float turnSpeed = 10;
 
 
     Transform model;
@@ -24,38 +24,20 @@ public class DroneController : MonoBehaviour
 
     void GetInput() {
 
-        //if (Input.GetAxis("Vertical") == 0) {
-        //    forwardTilt = Mathf.Lerp(forwardTilt, 0, Time.deltaTime * 1f);
-        //}
-        //else {
-        //    forwardTilt = Input.GetAxis("Vertical") * -40;
+        if (Input.GetAxis("Vertical") != 0) {
+            transform.Translate(-Vector3.forward * Input.GetAxis("Vertical"), Space.Self);
+        }
 
-        //    if (forwardTilt > 0) {
-        //        transform.Translate(Vector3.forward * forwardSpeed, Space.Self);
-        //    }
-        //    else {
-        //        transform.Translate(Vector3.forward * -forwardSpeed, Space.Self);
-        //    }
-        //}
+        if (Input.GetAxis("Horizontal") != 0) {
+            transform.Translate(-Vector3.right * Input.GetAxis("Horizontal"), Space.Self);
+        }
 
-        //if (Input.GetAxis("Horizontal") == 0) {
-        //    sideTilt = Mathf.Lerp(0, sideTilt, Time.deltaTime * 1f);
-        //}
-        //else {
-        //    sideTilt = Input.GetAxis("Horizontal") * 40;
-
-        //    if (sideTilt > 0) {
-        //        transform.Translate(Vector3.right * -forwardSpeed / 2, Space.Self);
-        //    }
-        //    else {
-        //        transform.Translate(Vector3.right * forwardSpeed / 2, Space.Self);
-        //    }
-        //}
-
-        //model.rotation = Quaternion.RotateTowards(model.rotation,
-        //    Quaternion.Euler(forwardTilt, 0, sideTilt), 5);
-
-        
+        if (Input.GetAxis("Turn") > 0) {
+            transform.Rotate(0, turnSpeed, 0);
+        }
+        if (Input.GetAxis("Turn") < 0) {
+            transform.Rotate(0, -turnSpeed, 0);
+        }
 
     }
 
