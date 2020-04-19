296517ba23f8efa19e0fef66b856e5f7b04c4c7d
James Smith <james_smith.games@live.co.uk>
2020-04-19 17:15:02 +0100

    Modelled coil

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -114,4 +114,9 @@ public class DroneController : MonoBehaviour
         //Balance();
 
     }
+
+    
+
+    
+
 }
---
+++ Assets/Scripts/Lightning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour {
+    ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+    void Start() {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other) {
+
+        Debug.Log("Particle hit");
+
+    }
+
+}
