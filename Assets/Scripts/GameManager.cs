1bd792baa3b5da6a3963eb9e910a419d57dc4ca0
James Smith <james_smith.games@live.co.uk>
2020-04-20 00:38:02 +0100

    Created valley model and grid texture.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Instance;
+
+    public int numOfCoils = 5;
+
+    static bool[] coilsActive;
+    public static bool[] CoilsActive {
+        get { return coilsActive; }
+    }
+
+    public List<Coil> coils;
+    public static List<Coil> Coils {
+        get { return Instance.coils; }
+    }
+
+   
+    
+
+    void Awake() {
+        Instance = this;
+    }
+
+    void Start() {
+        coilsActive = new bool[numOfCoils];
+    }
+
+    void UpdateCoils() {
+
+        //Randonly choose which coils to activate
+        for (int i = 0; i < numOfCoils; i++) {
+            if (Random.Range(1, 3) == 1) {
+                coilsActive[i] = true;
+            }
+            else
+                coilsActive[i] = false;
+        }
+    }
+
+    void Update() {
+
+
+    }
+}
---
+++ Assets/Scripts/TrackGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGenerator : MonoBehaviour
+{
+    public GameObject[] blocks;
+    public Transform startPos;
+    
+    Block currentBlock;
+    public int buffer = 20;
+
+    void Awake() {
+        GameObject startBlock = Instantiate(blocks[0], startPos.position, startPos.rotation, transform);
+        currentBlock = startBlock.GetComponent<Block>();
+        GeneratePath();
+    }
+
+    void GeneratePath() {
+        System.Random random = new System.Random();
+        string generatedPath = "";
+
+        for (int i = 0; i < buffer; i++) {  //create string of random integers
+            generatedPath += random.Next(1, blocks.Length).ToString();
+        }
+        
+        //Convert generated string of integers
+        char[] path = generatedPath.ToCharArray();  
+        for (int i = 0; i < path.Length; i++) {
+            int nextBlock = (int)Char.GetNumericValue(path[i]);
+
+            GameObject newBlock = Instantiate(blocks[nextBlock],
+                currentBlock.end.position, //place at end of current block
+                currentBlock.end.rotation, //match rotation at end
+                transform);                 //parent block to path object
+
+            //newBlock.transform.Rotate(90, 0, 0);
+            currentBlock = newBlock.GetComponent<Block>();
+        }
+
+    }
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
